12b508696911a1521b28fdbfa1693939df58b387
lhannelas <lhannelas@gmail.com>
2022-07-07 17:52:33 +0400

    updated animations

--- data.js
+++ data.js
@@ -25,3 +25,22 @@ let shopItemData = [{
   img: "Pictures/pexels-max-vakhtbovych-6758518.jpg"
 },
 ]
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
